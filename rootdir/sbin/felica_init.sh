b464890544e8bc8afc895b0d225837a66f9bc4f3
sakuramilk <c.sakuramilk@gmail.com>
2014-01-17 23:34:49 +0900

    update felica init script

--- rootdir/sbin/felica_init.sh
+++ rootdir/sbin/felica_init.sh
@@ -3,41 +3,28 @@
 #/sbin/setpropex ro.warranty_bit 0
 #/sbin/setpropex ro.emmc_checksum 0
 
-CMDLINE_FILE_JB_MR1=/data/media/0/kbc/cmdline
-if [ -f $CMDLINE_FILE_JB_MR1 ]; then
-    FELICA_CMDLINE=`cat $CMDLINE_FILE_JB_MR1`
+KBC_DATA_PATH=/data/media/0/kbc
+
+CMDLINE_FILE=$KBC_DATA_PATH/cmdline
+if [ -f $CMDLINE_FILE ]; then
+    FELICA_CMDLINE=`cat $CMDLINE_FILE`
     echo "$FELICA_CMDLINE" > /proc/cmdline
     exit 0
 fi
 
-FELICA_KEY_FILE_JB_MR1=/data/media/0/TweakS3A/felica_key
-if [ -f $FELICA_KEY_FILE_JB_MR1 ]; then
-    FELICA_KEY=`cat $FELICA_KEY_FILE_JB_MR1`
+FELICA_KEY_FILE=$KBC_DATA_PATH/felica_key
+if [ -f $FELICA_KEY_FILE ]; then
+    FELICA_KEY=`cat $FELICA_KEY_FILE`
     BASE_CMDLINE=`cat /proc/cmdline`
     echo "cordon=$FELICA_KEY $BASE_CMDLINE" > /proc/cmdline
+    exit 0
 fi
-
 
 # for old version
-CMDLINE_FILE_JB=/data/media/kbc/GNT2eXtremeTuner/cmdline
-if [ -f $CMDLINE_FILE_JB ]; then
-    FELICA_CMDLINE=`cat $CMDLINE_FILE_JB`
+CMDLINE_FILE=$KBC_DATA_PATH/GNT2eXtremeTuner/cmdline
+if [ -f $CMDLINE_FILE ]; then
+    FELICA_CMDLINE=`cat $CMDLINE_FILE`
     echo "$FELICA_CMDLINE" > /proc/cmdline
-    exit 0
-fi
-
-CMDLINE_FILE_JB_MR1=/data/media/0/kbc/GNT2eXtremeTuner/cmdline
-if [ -f $CMDLINE_FILE_JB_MR1 ]; then
-    FELICA_CMDLINE=`cat $CMDLINE_FILE_JB_MR1`
-    echo "$FELICA_CMDLINE" > /proc/cmdline
-    exit 0
-fi
-
-FELICA_KEY_FILE_JB=/data/media/TweakS3A/felica_key
-if [ -f $FELICA_KEY_FILE_JB ]; then
-    FELICA_KEY=`cat $FELICA_KEY_FILE_JB`
-    BASE_CMDLINE=`cat /proc/cmdline`
-    echo "cordon=$FELICA_KEY $BASE_CMDLINE" > /proc/cmdline
     exit 0
 fi
 
