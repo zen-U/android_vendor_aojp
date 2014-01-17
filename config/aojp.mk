5c1bb5ef9894af3134ef6a7f3c9cd86e0bf7381e
sakuramilk <c.sakuramilk@gmail.com>
2014-01-17 23:21:02 +0900

    set default locale ja-JP

--- config/aojp.mk
+++ config/aojp.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2014 The AOJP Project
+# Copyright (C) 2014 KBC Developers
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,11 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 LOCAL_PATH := vendor/aojp
+
+# Default locale
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.locale.language=ja \
+    ro.product.locale.region=JP
+
 # Init files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/sbin/felica_init.sh:root/sbin/felica_init.sh\
     $(LOCAL_PATH)/rootdir/sbin/setpropex:root/sbin/setpropex
-
-
