e363835ec1acb8cd9ae817f187bf44ce46b99216
sakuramilk <c.sakuramilk@gmail.com>
2015-03-26 17:32:42 +0900

    change d2dcm -> d2lte
    Change-Id: I4773265e3f3e24882750d21ead4d624f00e3971f

--- merge-cyanogenmod.sh
+++ merge-cyanogenmod.sh
@@ -30,19 +30,23 @@ func_fetch() {
     echo;
 }
 
+# platform
 func_fetch CyanogenMod android android
 func_fetch CyanogenMod android_build build
 func_fetch CyanogenMod android_frameworks_base frameworks/base
 func_fetch CyanogenMod android_packages_apps_Settings packages/apps/Settings
 func_fetch CyanogenMod android_frameworks_native frameworks/native
 func_fetch CyanogenMod android_system_core system/core
+
 # vendor
-func_fetch TheMuppets proprietary_vendor_samsung vendor/samsung
 func_fetch CyanogenMod android_vendor_cm vendor/cm
 
 # recovery
 func_fetch CyanogenMod android_bootable_recovery bootable/recovery
 
+#---------------------------------------
+# Samsung
+#---------------------------------------
 # device
 func_fetch CyanogenMod android_device_samsung_qcom-common device/samsung/qcom-common
 func_fetch CyanogenMod android_device_samsung_msm8660-common device/samsung/msm8660-common
@@ -52,19 +56,18 @@ func_fetch CyanogenMod android_device_samsung_smdk4412-common device/samsung/smd
 func_fetch CyanogenMod android_device_samsung_smdk4412-qcom-common device/samsung/smdk4412-qcom-common
 func_fetch CyanogenMod android_device_samsung_celox-common device/samsung/celox-common
 func_fetch CyanogenMod android_device_samsung_quincy-common device/samsung/quincy-common
-func_fetch CyanogenMod android_device_samsung_d2-common device/samsung/d2-common
 func_fetch CyanogenMod android_device_samsung_galaxys2-common device/samsung/galaxys2-common
 func_fetch CyanogenMod android_device_samsung_jf-common device/samsung/jf-common
 func_fetch CyanogenMod android_device_samsung_hlte device/samsung/hltedcm
 func_fetch CyanogenMod android_device_samsung_hlte-common device/samsung/hltedcm-common
-func_fetch CyanogenMod android_hardware_samsung hardware/samsung
 func_fetch CyanogenMod android_device_samsung_quincyatt device/samsung/quincydcm
 func_fetch CyanogenMod android_device_samsung_jflteatt device/samsung/jfltedcm
-func_fetch CyanogenMod android_device_samsung_d2att device/samsung/d2dcm
+func_fetch CyanogenMod android_device_samsung_d2lte device/samsung/d2lte
 func_fetch CyanogenMod android_device_samsung_i9305 device/samsung/sc03e
 func_fetch CyanogenMod android_device_samsung_i9100 device/samsung/sc02c
 func_fetch CyanogenMod android_device_samsung_t0lte device/samsung/sc02e
 func_fetch CyanogenMod android_device_samsung_skyrocket device/samsung/celoxdcm
+func_fetch CyanogenMod android_hardware_samsung hardware/samsung
 
 # kernel
 func_fetch CyanogenMod android_kernel_samsung_msm8660-common kernel/samsung/msm8660-dcm
@@ -73,8 +76,11 @@ func_fetch CyanogenMod android_kernel_samsung_smdk4412 kernel/samsung/exynos4412
 func_fetch CyanogenMod android_kernel_samsung_jf kernel/samsung/jfdcm kbc-aosp-kk
 func_fetch CyanogenMod android_kernel_samsung_hlte kernel/samsung/hlte
 
+# vendor
+func_fetch TheMuppets proprietary_vendor_samsung vendor/samsung
+
 #---------------------------------------
-#lge
+# lge
 #---------------------------------------
 func_fetch CyanogenMod android_device_lge_g2-common device/lge/g2-common
 
