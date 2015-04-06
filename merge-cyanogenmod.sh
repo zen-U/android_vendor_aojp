8a9a95baf17f6b1469acc48feba20f5d952737dd
ma34s <ma34s.sss@gmail.com>
2015-04-06 22:07:42 +0900

    update merge script
    Change-Id: Ibd85aa11b84508e49aad6ec5b3d98a8da060d308

--- merge-cyanogenmod.sh
+++ merge-cyanogenmod.sh
@@ -43,7 +43,7 @@ func_fetch CyanogenMod android_build build
 func_fetch CyanogenMod android_bionic bionic
 #func_fetch CyanogenMod android_frameworks_base frameworks/base
 #func_fetch CyanogenMod android_packages_apps_Settings packages/apps/Settings
-#func_fetch CyanogenMod android_frameworks_native frameworks/native
+func_fetch CyanogenMod android_frameworks_native frameworks/native
 #func_fetch CyanogenMod android_system_core system/core
 
 # vendor
