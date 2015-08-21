cce2d6476d6bb15ded9d03b1294de5e981cdd49f
sakuramilk <c.sakuramilk@gmail.com>
2015-08-21 19:47:38 +0900

    update build.sh
    Change-Id: I9d1806e99fdcb6359576bb477d0677c7d5027099

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 PRODUCT=$1
+MAKE_TARGET=$2
+
 PRODUCT_CONF=vendor/aojp/products/${PRODUCT}.conf
 if [ ! -e ${PRODUCT_CONF} ]; then
   echo "${PRODUCT_CONF} is not found."
@@ -19,7 +21,18 @@ echo " PRODUCT : ${PRODUCT}"
 echo "   CM_BUILDTYPE : ${CM_BUILDTYPE}"
 echo "   TARGET_RECOVERY : ${TARGET_RECOVERY}"
 echo "   PRODUCT_PREBUILT_WEBVIEWCHROMIUM : ${PRODUCT_PREBUILT_WEBVIEWCHROMIUM}"
+if [ ! "${MAKE_TARGET}" ]; then
+echo "   MAKE_TARGET : all"
+else
+echo "   MAKE_TARGET : ${MAKE_TARGET}"
+fi
 echo "========================================================================="
 
 . build/envsetup.sh
-brunch ${PRODUCT}
+
+if [ ! "${MAKE_TARGET}" ]; then
+  brunch ${PRODUCT}
+else
+  choosecombo release cm_${PRODUCT} userdebug
+  make ${MAKE_TARGET} -j4
+fi
