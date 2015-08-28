68817fc9d552b08fd28cf3912c6148baa97c2913
sakuramilk <c.sakuramilk@gmail.com>
2015-08-28 14:58:33 +0900

    update merge-cyanogenmod.sh
    Change-Id: Ic320b5014f115aafeccd1a8bf70daeadaf89f480

--- merge-cyanogenmod.sh
+++ merge-cyanogenmod.sh
@@ -8,6 +8,11 @@ func_fetch() {
     NAME=$2
     DIR=$3
     REMOTE_BRUNCH=$4
+    FETCH_REVISION=$5
+
+    if [ "$FETCH_REVISION" = "" ]; then
+        FETCH_REVISION=$REVISION
+    fi
 
     echo ================================================================
     echo "Project: $NAME"
@@ -15,15 +20,22 @@ func_fetch() {
     if [ ! -d $DIR ]; then
         echo "Skip"
     else
-      cd $DIR
-      git fetch https://github.com/$REMOTE/$NAME.git $REVISION
-      git merge FETCH_HEAD
-      if [ "$REMOTE_BRUNCH" != "" ]; then
-          git push github $REVISION:$REMOTE_BRUNCH
-      else
-          git push
-      fi
-      cd $ANDROID_HOME
+        cd $DIR
+
+        repo=`git remote -v |grep github.com/kbc-developers`
+        if [ -z "$repo" ]; then
+            echo "Skip: remote is not kbc-developers "
+        else
+            #Just kbc-developers repository
+            git fetch https://github.com/${REMOTE}/${NAME}.git ${FETCH_REVISION}
+            git merge FETCH_HEAD
+            if [ "$REMOTE_BRUNCH" != "" ]; then
+                git push github ${REVISION}:${REMOTE_BRUNCH}
+            else
+                git push github ${REVISION}
+            fi
+        fi
+        cd ${ANDROID_HOME}
     fi
 
     echo ================================================================
@@ -61,7 +73,7 @@ func_fetch CyanogenMod android_device_samsung_jf-common device/samsung/jf-common
 func_fetch CyanogenMod android_device_samsung_hlte device/samsung/hltedcm
 func_fetch CyanogenMod android_device_samsung_hlte-common device/samsung/hltedcm-common
 func_fetch CyanogenMod android_device_samsung_quincyatt device/samsung/quincydcm
-func_fetch CyanogenMod android_device_samsung_jflteatt device/samsung/jfltedcm
+func_fetch CyanogenMod android_device_samsung_jflte device/samsung/jflte
 func_fetch CyanogenMod android_device_samsung_d2lte device/samsung/d2lte
 func_fetch CyanogenMod android_device_samsung_i9305 device/samsung/sc03e
 func_fetch CyanogenMod android_device_samsung_i9100 device/samsung/sc02c
@@ -72,17 +84,21 @@ func_fetch CyanogenMod android_hardware_samsung hardware/samsung
 # kernel
 func_fetch CyanogenMod android_kernel_samsung_msm8660-common kernel/samsung/msm8660-dcm
 func_fetch CyanogenMod android_kernel_samsung_d2 kernel/samsung/d2dcm
-func_fetch CyanogenMod android_kernel_samsung_smdk4412 kernel/samsung/exynos4412dcm kbc-cyanogen-cm-11.0
-func_fetch CyanogenMod android_kernel_samsung_jf kernel/samsung/jfdcm kbc-aosp-kk
+func_fetch CyanogenMod android_kernel_samsung_smdk4412 kernel/samsung/exynos4412dcm
+func_fetch CyanogenMod android_kernel_samsung_jf kernel/samsung/jfdcm
 func_fetch CyanogenMod android_kernel_samsung_hlte kernel/samsung/hlte
 
 # vendor
 func_fetch TheMuppets proprietary_vendor_samsung vendor/samsung
 
 #---------------------------------------
-# lge
+# LGE
 #---------------------------------------
+# device
 func_fetch CyanogenMod android_device_lge_g2-common device/lge/g2-common
 
 #kernel
 func_fetch CyanogenMod  android_kernel_lge_msm8974 kernel/lge/msm8974
+
+# vendor
+func_fetch TheMuppets proprietary_vendor_lge vendor/lge
