d7af91e11c93bfbcbab2ef0d15811e4f1145754c
ma34s <ma34s.sss@gmail.com>
2015-10-25 01:20:00 +0900

    merge: support sc02e/sc03e
    Change-Id: I44faf7bfe48676a9e3a10e0a1cce91f23b540fe9

--- merge-cyanogenmod.sh
+++ merge-cyanogenmod.sh
@@ -86,17 +86,17 @@ func_fetch CyanogenMod android_device_samsung_jflteatt device/samsung/jfltedcm
 #func_fetch CyanogenMod android_device_samsung_hlte device/samsung/hltedcm
 #func_fetch CyanogenMod android_device_samsung_hlte-common device/samsung/hlte-common
 #func_fetch CyanogenMod android_hardware_samsung hardware/samsung
-#func_fetch CyanogenMod android_device_samsung_smdk4412-common device/samsung/smdk4412-common
-#func_fetch CyanogenMod android_device_samsung_smdk4412-qcom-common device/samsung/smdk4412-qcom-common
+func_fetch CyanogenMod android_device_samsung_smdk4412-common device/samsung/smdk4412-common
+func_fetch CyanogenMod android_device_samsung_smdk4412-qcom-common device/samsung/smdk4412-qcom-common
 #func_fetch CyanogenMod android_device_samsung_i9305 device/samsung/sc03e
-#func_fetch CyanogenMod android_device_samsung_t0lte device/samsung/sc02e
+func_fetch CyanogenMod android_device_samsung_t0lte device/samsung/sc02e
 func_fetch CyanogenMod android_device_samsung_klte-common device/samsung/klte-common
 func_fetch CyanogenMod android_device_samsung_klte device/samsung/kltedcm
 
 # kernel
 #func_fetch CyanogenMod android_kernel_samsung_msm8660-common kernel/samsung/msm8660-dcm
 #func_fetch CyanogenMod android_kernel_samsung_d2 kernel/samsung/d2dcm
-#func_fetch CyanogenMod android_kernel_samsung_smdk4412 kernel/samsung/exynos4412dcm
+func_fetch CyanogenMod android_kernel_samsung_smdk4412 kernel/samsung/exynos4412dcm
 func_fetch CyanogenMod android_kernel_samsung_jf kernel/samsung/jfdcm
 #func_fetch CyanogenMod android_kernel_samsung_hlte kernel/samsung/hlte
 func_fetch CyanogenMod android_kernel_samsung_klte kernel/samsung/klte
