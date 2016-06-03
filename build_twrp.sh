856a45b7c7ddbefb6b63782cee05d788442caa04
ma34s <ma34s.sss@gmail.com>
2016-06-03 16:43:57 +0900

    trwp build/ add variable "twrp version"
    Change-Id: Iccbe23b28f5d7f39a0e5e157d5afd23cf766cc25

--- build_twrp.sh
+++ build_twrp.sh
@@ -7,4 +7,5 @@
 PRODUCT=$1
 
 export TARGET_RECOVERY=twrp
+export TWRP_VERSION=`cat $(pwd)/bootable/recovery-twrp/variables.h |grep TW_VERSION_STR | awk '{print $3;}' |sed -e s/\"//g`
 ./build.sh $PRODUCT recoveryimage ${@:2}
